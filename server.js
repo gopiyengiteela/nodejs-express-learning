ef132d6e3e47094cb724289f4c0e1fe94a8c8d37
gopiyengiteela <gopi.yengiteela2@gmailcom>
2020-06-11 22:30:45 -0700

    rest apis

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const path = require("path");
 const cookieSession = require("cookie-session");
+const createError = require("http-errors");
+
+const bodyParser = require("body-parser");
 
 const FeedbackService = require("./services/FeedbackService");
 const SpeakersService = require("./services/SpeakerService");
@@ -8,9 +11,11 @@ const SpeakersService = require("./services/SpeakerService");
 const feedbackService = new FeedbackService("./data/feedback.json");
 const speakersService = new SpeakersService("./data/speakers.json");
 
+const routes = require("./routes");
+
 const app = express();
 
-const routes = require("./routes");
+app.locals.siteName = "ROUX Academy";
 
 const port = 3000;
 
@@ -19,11 +24,14 @@ app.set("trust proxy", 1);
 app.use(
   cookieSession({
     name: "session",
-    keys: ["sdgshs", "shdgasd"],
+    keys: ["Ghdur687399s7w", "hhjjdf89s866799"],
   })
 );
 
-app.set("View Engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "./views"));
 
 app.locals.siteName = "ROUX Meetups";
@@ -62,5 +70,5 @@ app.use((err, request, response, next) => {
 });
 
 app.listen(port, () => {
-  console.log(`Express is listening at ${port}`);
+  console.log(`Express server listening on port ${port}!`);
 });
--- static/js/pixgrid.js
+++ static/js/pixgrid.js
@@ -1,32 +1,36 @@
-const pixGrid = (function() {
+const pixGrid = (function () {
   function centerImage(theImage) {
     let myDifX = (window.innerWidth - theImage.width) / 2;
-            var myDifY = (window.innerHeight - theImage.height) / 2;
-    return (theImage.style.top = `${myDifY  }px`), (theImage.style.left = `${myDifX  }px`), theImage;
+    var myDifY = (window.innerHeight - theImage.height) / 2;
+    return (
+      (theImage.style.top = `${myDifY}px`),
+      (theImage.style.left = `${myDifX}px`),
+      theImage
+    );
   }
-  let myNode = document.querySelector('.pixgrid');
+  let myNode = document.querySelector(".pixgrid");
   myNode.addEventListener(
-    'click',
-    function(e) {
-      if (e.target.tagName === 'IMG') {
-        let myOverlay = document.createElement('div');
-        (myOverlay.id = 'overlay'),
+    "click",
+    function (e) {
+      if (e.target.tagName === "IMG") {
+        let myOverlay = document.createElement("div");
+        (myOverlay.id = "overlay"),
           document.body.appendChild(myOverlay),
-          (myOverlay.style.position = 'absolute'),
+          (myOverlay.style.position = "absolute"),
           (myOverlay.style.top = 0),
-          (myOverlay.style.backgroundColor = 'rgba(0,0,0,0.7)'),
-          (myOverlay.style.cursor = 'pointer'),
-          (myOverlay.style.width = `${window.innerWidth  }px`),
-          (myOverlay.style.height = `${window.innerHeight  }px`),
-          (myOverlay.style.top = `${window.pageYOffset  }px`),
-          (myOverlay.style.left = `${window.pageXOffset  }px`);
+          (myOverlay.style.backgroundColor = "rgba(0,0,0,0.7)"),
+          (myOverlay.style.cursor = "pointer"),
+          (myOverlay.style.width = `${window.innerWidth}px`),
+          (myOverlay.style.height = `${window.innerHeight}px`),
+          (myOverlay.style.top = `${window.pageYOffset}px`),
+          (myOverlay.style.left = `${window.pageXOffset}px`);
         let imageSrc = e.target.src;
-                    var largeImage = document.createElement('img');
-        (largeImage.id = 'largeImage'),
-          (largeImage.src = `${imageSrc.substr(0, imageSrc.length - 7)  }.jpg`),
-          (largeImage.style.display = 'block'),
-          (largeImage.style.position = 'absolute'),
-          largeImage.addEventListener('load', function() {
+        var largeImage = document.createElement("img");
+        (largeImage.id = "largeImage"),
+          (largeImage.src = `${imageSrc.substr(0, imageSrc.length - 7)}.jpg`),
+          (largeImage.style.display = "block"),
+          (largeImage.style.position = "absolute"),
+          largeImage.addEventListener("load", function () {
             this.height > window.innerHeight &&
               ((this.ratio = window.innerHeight / this.height),
               (this.height = this.height * this.ratio),
@@ -39,32 +43,32 @@ const pixGrid = (function() {
               myOverlay.appendChild(largeImage);
           }),
           largeImage.addEventListener(
-            'click',
-            function() {
+            "click",
+            function () {
               myOverlay &&
-                (window.removeEventListener('resize', window, !1),
-                window.removeEventListener('scroll', window, !1),
+                (window.removeEventListener("resize", window, !1),
+                window.removeEventListener("scroll", window, !1),
                 myOverlay.parentNode.removeChild(myOverlay));
             },
             !1
           ),
           window.addEventListener(
-            'scroll',
-            function() {
+            "scroll",
+            function () {
               myOverlay &&
-                ((myOverlay.style.top = `${window.pageYOffset  }px`),
-                (myOverlay.style.left = `${window.pageXOffset  }px`));
+                ((myOverlay.style.top = `${window.pageYOffset}px`),
+                (myOverlay.style.left = `${window.pageXOffset}px`));
             },
             !1
           ),
           window.addEventListener(
-            'resize',
-            function() {
+            "resize",
+            function () {
               myOverlay &&
-                ((myOverlay.style.width = `${window.innerWidth  }px`),
-                (myOverlay.style.height = `${window.innerHeight  }px`),
-                (myOverlay.style.top = `${window.pageYOffset  }px`),
-                (myOverlay.style.left = `${window.pageXOffset  }px`),
+                ((myOverlay.style.width = `${window.innerWidth}px`),
+                (myOverlay.style.height = `${window.innerHeight}px`),
+                (myOverlay.style.top = `${window.pageYOffset}px`),
+                (myOverlay.style.left = `${window.pageXOffset}px`),
                 centerImage(largeImage));
             },
             !1
